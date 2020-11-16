37b44e7ce8452f8d74deb9d99d56df09cf34e308
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-15 19:50:53 -0800

    added the requiset files

---
+++ portfolio/pages/contact.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import Navbar from '../components/navbar';
+import Jumbotron from '../components/jumbotron';
+import Card from '../components/card';
+import Footer from '../components/footer';
+import '../static/css/style.css';
---
+++ portfolio/pages/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import Jumbotron from '../components/jumbotron';
+import Picture from '../components/picture';
+import Footer from '../components/footer';
+import '../static/css/style.css';
