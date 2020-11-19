e808870909dacd1e1094b298668bd220c1bd7f95
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-11-18 19:40:33 -0800

    finished writing about projects

--- src/components/Projects/Drinks.js
+++ src/components/Projects/Drinks.js
@@ -7,7 +7,7 @@ function Cocktails() {
         <div className="card-Image">
           <h5 class="card-title">Drink to the Weather</h5>
         
-            <p class="card-text">An at home bartender app built for my first group project</p>
+            <p class="card-text">An at home bartender app built for my first group project. We used the OpenWeather API and the Cocktail Db Api in conjunction with jquerry, bouncer.js and Foundation to build recommend drinks based on the temperature of your area.</p>
         <a href="https://github.com/RKSouth/drink-to-the-weather">Github Link</a>
         <span> || </span>
           <a href="https://rksouth.github.io/drink-to-the-weather/">Live Deployment</a>
--- src/components/Projects/EDB.js
+++ src/components/Projects/EDB.js
@@ -7,7 +7,7 @@ function Burgers() {
         <div className="card-Image">
           <h5 class="card-title">Eat-Da-Burgers</h5>
         
-            <p class="card-text">A simple way to track how you eat burgers but with a complex set of code</p>
+            <p class="card-text">A simple way to track how you eat burgers but with a complex set of code. Although it may seem simple this project uses express and handlebars to store data in a mysql database. It also utilized JAWSDB and heroku for deployment.</p>
         <a href="https://github.com/RKSouth/NodeExpressHandlebars">Github Link</a>
         <span> || </span>
           <a href="https://lit-garden-17610.herokuapp.com/">Live Deployment</a>
