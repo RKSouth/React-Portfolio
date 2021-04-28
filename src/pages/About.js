2dc0e035b2b380c298d741b20cf57710d181db94
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-04-28 10:44:06 -0700

    Updated About me section

--- src/pages/About.js
+++ src/pages/About.js
@@ -15,8 +15,8 @@ function About() {
                 {/* <img src={Me} alt="woman with glasses" className="img-fluid"/> */}
              </div>
              <div className="card-text"  className="single-post-content-wrapper p-3">
-             <p> I am currently a student at UC Berkeley's Extension Full Stack Web Developer Certificate Program. I have BS in
-              Environmental Science and Chemistry from The Evergreen State College that I received in 2011. I have passion for solving problems using logic and critical thinking. I have never been afraid big projects or big problems and have yet to meet one that I can't find a creative solution for. I like to find the beauty in an algorithm or a formula. I am ready to apply my methodical and reasoned approach to anything put in front of me. 
+             <p> I recently received a Certificate in Full Stack Web Development from UC Berkeley Extension. I have also taken courses in design, photo editing, python and graphql.  I have BS in
+              Environmental Science and Chemistry from The Evergreen State College that I received in 2011. I have passion for solving problems using logical steps and critical thinking.
             
             </p>      
             <p>
