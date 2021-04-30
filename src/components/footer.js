a416695392a69ac9ea051569055bb3a68511378d
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-04-29 18:26:18 -0700

    added MIT Licence and Github repo link to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,16 +5,25 @@ import './Compstyle.css'
 function Footer() {
   return (
     <footer>
-  <div className="container col-sm-10 col-md-8 col-lg-8">
-    <nav className="navbar navbar-expand-md">
-      <a className="navbar-brand "><light><p>&#169; Copyright Rachael Kelm-Southworth</p></light></a>
+      <div className="container col-sm-10 col-md-8 col-lg-8">
+        <nav className="navbar navbar-expand-md">
+          <div className="footer-copyright text-center py-3 text-muted">
+            © 2021 Copyright:
+          <a href="https://choosealicense.com/licenses/mit/">MIT licenses</a>
+         
+            <p>Go to Portfolio <a
+              href="https://github.com/RKSouth/React-Portfolio"
+              target="_blank"
+              rel="noreferrer"
+            >Github</a> 
+            </p>
 
-      <div className="collapse navbar-collapse" id="navbarNav">
+          </div>
+
+        </nav>
       </div>
-    </nav>
-    </div>
 
-</footer>
+    </footer >
   )
 
 
