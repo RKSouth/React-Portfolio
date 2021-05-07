5cdcbf851f2971e99c4ad7792821418deacd7267
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-05-07 14:39:34 -0700

    fixed image

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from "./Navbar"
-import Me from './Images/ME.jpg';
+import Me from './Images/rachael.jpg';
 import './Compstyle.css'
 
 function Jumbotron() {
@@ -13,7 +13,7 @@ function Jumbotron() {
         <div className="jumbotron jumbotron-fluid">
            <div className="container">
              <h1 className="display-4">Rachael Kelm-Southworth</h1>
-             <img src={Me} alt="woman" className="img-fluid"/>
+             <img src={Me} alt="woman" className="img-fluid" id="myPicture"/>
              <p className="lead" id ="About">Full-Stack Web Developer</p >
            </div>
       </div>
