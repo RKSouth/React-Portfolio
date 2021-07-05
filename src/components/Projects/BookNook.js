2b19a0fa204234ac1e2503f6e9376d3f5e7917ea
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2021-07-05 14:00:01 -0700

    Made "TheBookNook" Card

--- src/components/Projects/BookNook.js
+++ src/components/Projects/BookNook.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import AxelImage from "../Images/TheBookNook.png"
-import Code from "../Images/AxelTheBird-Code.png"
+import BookImage from "../Images/TheBookNook.png"
+import Code from "../Images/TheBookNook-Code.png"
 import '../Compstyle.css'
 
 function Books() {
@@ -8,9 +8,9 @@ function Books() {
         <div className="card  col-md-6 p-3">
         <div className="card-Image">
           <h5 className="card-title">The Book Nook</h5>
-          <a href="https://github.com/RKSouth/AxelTheBird">Github Link</a>
+          <a href="https://github.com/RKSouth/Book-Search">Github Link</a>
             <span> || </span>
-              <a href="https://rksouth.github.io/AxelTheBird/">Live Deployment</a>
+              <a href="https://pure-dawn-66807.herokuapp.com/">Live Deployment</a>
               <hr/>
         <div id="carouselBook" className="carousel slide" data-ride="carousel">
           <ol className="carousel-indicators">
@@ -20,16 +20,14 @@ function Books() {
           </ol>
           <div className="carousel-inner">
             <div className="carousel-item active">
-              <img className="card-Image-fluid" src={AxelImage} alt="Home Page"/>
+              <img className="card-Image-fluid" src={BookImage} alt="Home Page"/>
             </div>
             <div className="carousel-item">
               <img className="card-Image-fluid" src={Code} alt="Second slide"/>
             </div>
             <div className="carousel-item">
-               <p className="card-text">This project, built in p5.js is the story of Axel the Bird. 
-               It used switch and if statements to follow a somewhat randomly generated story that shows images. 
-               I drew most of the images myself and got a few from websites that have free images. 
-               I wrote the music and recorded it using my electronic piano (keyboard) and the simple recording device on my computer.  </p>
+               <p className="card-text">Using React, MongoDB, Express.js and the google books API I built this project that helps you find and save awesome books to read. 
+               Every book lover needs a special book nook to keep their books so here it is! This is a CRUD App that writes to a database, deployed using heroku and uses React hooks, props and states. </p>
             </div>
           </div>
                 <a className="carousel-control-prev" href="#carouselBook" role="button" data-slide="prev">
